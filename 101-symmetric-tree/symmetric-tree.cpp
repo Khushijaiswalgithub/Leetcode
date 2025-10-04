52177292a7285498ee2478511658332f25a3a8fb
Khushi Jaiswal <159470469+Khushijaiswalgithub@users.noreply.github.com>
2025-10-04 11:16:50 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (10.64%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -16,14 +16,11 @@ public:
         
     }
     bool Symmetric(TreeNode* left,TreeNode* right){    
-        if(left==NULL||right==NULL){
+        if(left==NULL || right==NULL){
             return left==right;
         }
-        if(left->val!=right->val){
-            return false;
-            
-        }
-        return Symmetric(left->left,right->right)&&Symmetric(left->right,right->left); 
+        if(left->val!=right->val) return false;
+        return Symmetric(left->left,right->right) && Symmetric(left->right,right->left);
     }
 
 };
